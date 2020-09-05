1bda78ed5ba11a1f92822c84d0ebfb552ceb0e9e
JoeRoot0306 <70695822+JoeRoot0306@users.noreply.github.com>
2020-09-05 02:40:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
 <?php
-  include("store.php");
   include("fetch.php");
 ?>
 
 <html>
  <div class = "form">
-    <form action = "index.php" method = "POST">
-      <label>Enter your todo description: <br></label>
-      <input type="text" name="description" value = "<?php echo $description?>;">
-      <div style="color:red"><?php echo $errors['description']; ?></div>
+    <form action = "store.php" method = "POST">
+      <label>Enter your todo description:<br></label>
+      <input type="text" name="description" value =><br>
       <label>Place:<br></label>
-      <input type="text" name="place" value = "<?php echo $place?>;">
-      <div style="color:red"><?php echo $errors['place']; ?></div>
+      <input type="text" name="place" value = ><br>
       <label>Time<br></label>
-      <input type="date" name="time" value = "<?php echo $time?>;">
-      <div style="color:red"><?php echo $errors['time']; ?></div>
+      <input type="date" name="time" value = ><br>
       <input type="submit" name="submit" value="submit">
     </form>
   </div>
@@ -25,12 +21,14 @@
         <th>Description</th>
         <th>Place</th>
         <th>Time</th>
+        <th>Info</th>
       </tr>
       <?php foreach($data as $result){ ?>
       <tr>
         <td><?php echo $result['description'];?></td>
         <td><?php echo $result['place'];?></td>
         <td><?php echo $result['time'];?></td>
+        <td><a href=more-info.php>More info</a></td>
       </tr>
       <?php } ?>
     </table>
